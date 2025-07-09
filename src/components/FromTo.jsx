ccc1f7aa1d283e4ff3a0d5e2da06f5059a467aae
Sanskar Jain <sanskarj257@gmail.com>
2025-07-09 13:08:51 +0530

    Baisc revert

--- src/components/FromTo.jsx
+++ src/components/FromTo.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import './FromTo.module.css';
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
@@ -6,22 +6,46 @@ import gsap from 'gsap';
 export default function FromTo() {
 
     const container = useRef();
+    const boxRef = useRef();
+    const [xPostion, setXPosition] = useState(0);
+    const [yPosition, setYPosition] = useState(0);
+    const [rotationValue, setRotationValue] = useState(0);
+
+    const randomXValue = gsap.utils.random(0, 250, 50);
+    const randomYValue = gsap.utils.random(0, 250, 50);
+    const randomRValue = gsap.utils.random(0, 800, 50);
 
     useGSAP(() => {
-        gsap.from(".box", {
-            rotate: 360,
-            scale: 0,
-            opacity: 0,
-            delay: 1,
-            duration: 1
-        })
-    }, { scope: container })
+        gsap.to(boxRef.current, {
+            x: xPostion,
+            y: yPosition,
+            duration: 1,
+            rotate: rotationValue
+        });
+    }, { dependencies: [xPostion, yPosition, rotationValue], revertOnUpdate: true });
+
+    // useGSAP(() => {
+    //     gsap.from(".box", {
+    //         rotate: 360,
+    //         scale: 0,
+    //         opacity: 0,
+    //         delay: 1,
+    //         duration: 1
+    //     })
+    // }, { scope: container })
 
     return (
         <section>
             <div className="container" ref={container}>
-                <div className="box">Box 1</div>
-                <div className="circle">Circle 1</div>
+                <div className="box" ref={boxRef}>Box 1</div>
+                {/* <div className="circle">Circle 1</div> */}
+                <button
+                    className='btn'
+                    onClick={() => {
+                        setXPosition(randomXValue)
+                        setYPosition(randomYValue)
+                        setRotationValue(randomRValue)
+                    }}>Move</button>
             </div>
         </section>
     )
