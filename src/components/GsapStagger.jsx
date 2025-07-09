7bb3187cc6239c0fba7ae6ac6d5cf91a7f9953e7
Sanskar Jain <sanskarj257@gmail.com>
2025-07-09 14:40:42 +0530

    Stagger

---
+++ src/components/GsapStagger.jsx
@@ -0,0 +1,34 @@
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap';
+import React from 'react'
+
+export default function GsapStagger() {
+
+    useGSAP(() => {
+        gsap.to('.stagger-box', {
+            y: 300,
+            rotation: 90,
+            borderRadius: '100%',
+            repeat: -1,
+            yoyo: true,
+
+            stagger: {
+                amount: 1.5,
+                ease: 'circ'
+            }
+        })
+    });
+
+
+  return (
+    <section>
+      <h2>Stagger</h2>
+      <div className="slide">
+        <div className="redBox stagger-box"></div>
+        <div className="redBox stagger-box"></div>
+        <div className="redBox stagger-box"></div>
+        <div className="redBox stagger-box"></div>
+      </div>
+    </section>
+  )
+}
